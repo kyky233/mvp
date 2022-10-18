80b2324c4d5524c7244f24837f181f459d1c6e80
kyky233 <danqi.allison.yan@gmail.com>
2022-10-19 07:10:06 +0800

    /

--- lib/models/util/misc.py
+++ lib/models/util/misc.py
@@ -34,7 +34,8 @@ import mmcv
 import os.path as osp
 import shutil
 
-if float(torchvision.__version__[:3]) < 0.5:
+# if float(torchvision.__version__[:3]) < 0.5:
+if float(torchvision.__version__.split('/')[1]) < 5:
     import math
     from torchvision.ops.misc import _NewEmptyTensorOp
 
@@ -69,7 +70,8 @@ if float(torchvision.__version__[:3]) < 0.5:
             int(math.floor(input.size(i + 2) * scale_factors[i]))
             for i in range(dim)
         ]
-elif float(torchvision.__version__[:3]) < 0.7:
+# elif float(torchvision.__version__[:3]) < 0.7:
+elif float(torchvision.__version__.split('/')[1]) < 7:
     from torchvision.ops import _new_empty_tensor
     from torchvision.ops.misc import _output_size
 
@@ -557,7 +559,8 @@ def interpolate(input, size=None,
     This will eventually be supported natively by PyTorch, and this
     class can go away.
     """
-    if float(torchvision.__version__[:3]) < 0.7:
+    # if float(torchvision.__version__[:3]) < 0.7:
+    if float(torchvision.__version__.split('/')[1]) < 7:
         if input.numel() > 0:
             return torch.nn.functional.interpolate(
                 input, size, scale_factor, mode, align_corners
@@ -565,7 +568,8 @@ def interpolate(input, size=None,
 
         output_shape = _output_size(2, input, size, scale_factor)
         output_shape = list(input.shape[:-2]) + list(output_shape)
-        if float(torchvision.__version__[:3]) < 0.5:
+        # if float(torchvision.__version__[:3]) < 0.5:
+        if float(torchvision.__version__.split('/')[1]) < 5:
             return _NewEmptyTensorOp.apply(input, output_shape)
         return _new_empty_tensor(input, output_shape)
     else:
